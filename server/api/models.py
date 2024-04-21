3f2404bc194718e9250b2e8213032d258d4ab61d
Dan G <daniel.w.gray1994@gmail.com>
2024-04-21 11:02:40 -0400

    added another progress tracking model for the user; migrated changes to the datatbase

--- server/api/models.py
+++ server/api/models.py
@@ -16,17 +16,17 @@ class QuestStep(models.Model):
 
 class Quest(models.Model):
     title = models.CharField(max_length=200)
-    steps = models.ManyToManyField(QuestStep, on_delete=models.CASCADE, related_name='quest_steps')
+    steps = models.ManyToManyField(QuestStep, related_name='quest_steps')
     bosses = models.ManyToManyField(Boss, related_name="bosses")
     items_required = models.ManyToManyField(Item, related_name="items_required")
     items_rewarded = models.ManyToManyField(Item, related_name="items_rewarded")
 
 
 class CustomUser(AbstractUser):
-    quest_steps_progress = models.ManyToManyField(QuestStep, through='UserStepProgress')
-    quests_in_proress = models.ManyToManyField(Quest, through='UserQuestProgress')
-    quests_completed = models.ManyToManyField(Quest, through='UserQuestProgress')
-    bosses_defeated = models.ManyToManyField(Boss, through='UserBossDefeated')
+    quest_steps_progress = models.ManyToManyField(QuestStep, through='UserStepProgress', related_name='quest_steps_progress')
+    quests_in_proress = models.ManyToManyField(Quest, through='UserQuestProgress', related_name='quests_in_progress')
+    quests_completed = models.ManyToManyField(Quest, through='UserQuestCompleted', related_name='quests_completed')
+    bosses_defeated = models.ManyToManyField(Boss, through='UserBossDefeated', related_name='bosses_defeated')
 
 
 class UserStepProgress(models.Model):
@@ -39,8 +39,12 @@ class UserQuestProgress(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     quest = models.ForeignKey(Quest, on_delete=models.CASCADE)
     in_progress = models.BooleanField(default=False)
-    completed = models.BooleanField(default=False)
+    
 
+class UserQuestCompleted(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    quest = models.ForeignKey(Quest, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
 
 class UserBossDefeated(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
