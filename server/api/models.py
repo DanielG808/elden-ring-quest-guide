bc94628733df640a4d76ced27edea45859fb5122
Dan G <daniel.w.gray1994@gmail.com>
2024-04-21 10:37:37 -0400

    added Item, Boss, QuestStep, and Quest models to the db

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Boss(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class QuestStep(models.Model):
+    step = models.CharField(max_length=500)
+
+
+class Quest(models.Model):
+    title = models.CharField(max_length=200)
+    steps = models.ManyToManyField(QuestStep, on_delete=models.CASCADE, related_name='quest_steps')
+    bosses = models.ManyToManyField(Boss, related_name="bosses")
+    items_required = models.ManyToManyField(Item, related_name="items_required")
+    items_rewarded = models.ManyToManyField(Item, related_name="items_rewared")
+
+    
+
+
