87c7b5948365b99db9d4be89d5a7f1c26429f5ad
Dan G <daniel.w.gray1994@gmail.com>
2024-04-21 10:55:38 -0400

    added a CustomUser model and progress tracking models for the user

--- server/api/models.py
+++ server/api/models.py
@@ -19,7 +19,34 @@ class Quest(models.Model):
     steps = models.ManyToManyField(QuestStep, on_delete=models.CASCADE, related_name='quest_steps')
     bosses = models.ManyToManyField(Boss, related_name="bosses")
     items_required = models.ManyToManyField(Item, related_name="items_required")
-    items_rewarded = models.ManyToManyField(Item, related_name="items_rewared")
+    items_rewarded = models.ManyToManyField(Item, related_name="items_rewarded")
+
+
+class CustomUser(AbstractUser):
+    quest_steps_progress = models.ManyToManyField(QuestStep, through='UserStepProgress')
+    quests_in_proress = models.ManyToManyField(Quest, through='UserQuestProgress')
+    quests_completed = models.ManyToManyField(Quest, through='UserQuestProgress')
+    bosses_defeated = models.ManyToManyField(Boss, through='UserBossDefeated')
+
+
+class UserStepProgress(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    quest_step = models.ForeignKey(QuestStep, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+
+
+class UserQuestProgress(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    quest = models.ForeignKey(Quest, on_delete=models.CASCADE)
+    in_progress = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
+
+
+class UserBossDefeated(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    boss = models.ForeignKey(Boss, on_delete=models.CASCADE)
+    defeated = models.BooleanField(default=False)
+    # Perhaps we add an 'attempts' field if we wanna track that later?
 
     
 
