a4c3f73202af565d0294f7383803ef7a384e9609
Calvin Roberts <you@example.com>
2024-05-05 14:23:38 -0600

    added header and home page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'gold': "#CDAF6B",
+      'blue': "#073036",
+    },
   },
   plugins: [],
 };
